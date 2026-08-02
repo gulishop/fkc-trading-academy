824cef131403ea4e8f674394a76db0726a6563d7
gulishop <135118720+gulishop@users.noreply.github.com>
2026-08-02 16:14:46 +0500

    Add Google Ads script to generate_post.py

--- generate_post.py
+++ generate_post.py
@@ -546,6 +546,7 @@ HEAD = """<!DOCTYPE html>
 <meta property="og:description" content="{ogdesc}">
 <meta property="og:image" content="{ogimage}">
 <meta name="twitter:card" content="summary_large_image">
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5639688573760714" crossorigin="anonymous"></script>
 <link rel="icon" href="{logo_href}">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link href="https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,500;9..144,700&family=IBM+Plex+Mono:wght@400;500;600&display=swap" rel="stylesheet">
